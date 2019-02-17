db8d18b0f50a207cb207813998a612498e7fd731
Administrator <shiwei_aiting@sina.com>
2019-02-18 00:14:31 +0800

    测试借口类TestController提交

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	
+	@RequestMapping("index")
+	public String index(){
+		return "index";
+	}
+}
